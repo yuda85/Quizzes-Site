edff0d17929a6e4b05f83fd982e1bc4d941eaea2
didimango <53489397+didimango@users.noreply.github.com>
2021-09-10 15:22:20 +0300

    fixed

--- Quizzes-Site/src/app/services/auth.service.ts
+++ Quizzes-Site/src/app/services/auth.service.ts
@@ -1,22 +1,88 @@
 import { Injectable } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
-import { AngularFirestore } from '@angular/fire/firestore';
-import * as firebase from 'firebase'
+import {
+  AngularFirestore,
+  AngularFirestoreDocument,
+} from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import * as firebase from 'firebase';
+import { BehaviorSubject, Observable, Subject } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
+  private isLoggedIn$: BehaviorSubject<boolean> = new BehaviorSubject<boolean>(
+    false
+  );
+  private userDetails$: Subject<any> = new Subject<any>();
 
-  constructor(private afs: AngularFirestore, private afAuth: AngularFireAuth) {}
+  constructor(
+    private afs: AngularFirestore,
+    private afAuth: AngularFireAuth,
+    private router: Router
+  ) {
+    const savedUserString = localStorage.getItem('user');
+    if (savedUserString !== null) {
+      this.isLoggedIn$.next(true);
+    }
 
-  public signInWithGoogle(){
-    this.authLogin(new firebase.default.auth.GoogleAuthProvider())
+    afAuth.authState.subscribe((user) => {
+      if (!!user) {
+        this.userDetails$.next(<any>user);
+        const userString: string = JSON.stringify(user);
+        localStorage.setItem('user', userString);
+        this.isLoggedIn$.next(true);
+      } else {
+        localStorage.removeItem('user');
+        this.isLoggedIn$.next(false);
+      }
+    });
   }
 
-  private authLogin(provider: firebase.default.auth.AuthProvider){
-    return this.afAuth.signInWithPopup(provider).then( (res) =>{
-      console.log(res);
+  public signInWithGoogle() {
+    this.authLogin(new firebase.default.auth.GoogleAuthProvider());
+  }
+
+  public signOut(): Promise<void> {
+    return this.afAuth.signOut().then(() => {
+      localStorage.removeItem('user');
+      this.router.navigate(['/']);
+      this.userDetails$.next(undefined);
+    });
+  }
+
+  public isLoggedIn(): Observable<boolean> {
+    return this.isLoggedIn$.asObservable();
+  }
+
+  public getUserData(): Observable<any> {
+    return this.userDetails$.asObservable();
+  }
+
+  private authLogin(provider: firebase.default.auth.AuthProvider) {
+    return this.afAuth.signInWithPopup(provider).then((res) => {
+      this.isLoggedIn$.next(true);
+      this.setUserData(res.user as any);
+      this.router.navigate(['chat']);
+    });
+  }
+
+  private setUserData(user?: any): Promise<void> | void {
+    if (!user) return;
+    const userRef: AngularFirestoreDocument<any> = this.afs.doc(
+      `users/${user.uid}`
+    );
+
+    const userData: any = {
+      uid: user.uid,
+      email: user.email,
+      displayName: user.displayName,
+      photoURL: user.photoURL,
+    };
+
+    return userRef.set(userData, {
+      merge: true,
     });
   }
 }
